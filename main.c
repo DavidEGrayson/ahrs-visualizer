88ae017b4a1a9b8268d9f6b4033a7be3a1181eef
David Grayson <davidegrayson@gmail.com>
2012-07-04 00:38:53 -0700

    Better indentation

--- main.c
+++ main.c
@@ -36,139 +36,139 @@ EGLContext context;
 // Sets the display, OpenGL|ES context and screen stuff
 static void init_opengl(void)
 {
-   EGLBoolean result;
-   EGLint num_config;
+    EGLBoolean result;
+    EGLint num_config;
 
-   static EGL_DISPMANX_WINDOW_T nativewindow;
+    static EGL_DISPMANX_WINDOW_T nativewindow;
 
-   DISPMANX_ELEMENT_HANDLE_T dispman_element;
-   DISPMANX_DISPLAY_HANDLE_T dispman_display;
-   DISPMANX_UPDATE_HANDLE_T dispman_update;
+    DISPMANX_ELEMENT_HANDLE_T dispman_element;
+    DISPMANX_DISPLAY_HANDLE_T dispman_display;
+    DISPMANX_UPDATE_HANDLE_T dispman_update;
 
-   static const EGLint attribute_list[] =
-   {
-      EGL_RED_SIZE, 8,
-      EGL_GREEN_SIZE, 8,
-      EGL_BLUE_SIZE, 8,
-      EGL_ALPHA_SIZE, 8,
-      EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
-      EGL_NONE
-   };
+    static const EGLint attribute_list[] =
+        {
+            EGL_RED_SIZE, 8,
+            EGL_GREEN_SIZE, 8,
+            EGL_BLUE_SIZE, 8,
+            EGL_ALPHA_SIZE, 8,
+            EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
+            EGL_NONE
+        };
    
-   EGLConfig config;
+    EGLConfig config;
 
-   // get an EGL display connection
-   display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
-   assert(display!=EGL_NO_DISPLAY);
+    // get an EGL display connection
+    display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
+    assert(display!=EGL_NO_DISPLAY);
 
-   // initialize the EGL display connection
-   result = eglInitialize(display, NULL, NULL);
-   assert(EGL_FALSE != result);
+    // initialize the EGL display connection
+    result = eglInitialize(display, NULL, NULL);
+    assert(EGL_FALSE != result);
 
-   // get an appropriate EGL frame buffer configuration
-   result = eglChooseConfig(display, attribute_list, &config, 1, &num_config);
-   assert(EGL_FALSE != result);
+    // get an appropriate EGL frame buffer configuration
+    result = eglChooseConfig(display, attribute_list, &config, 1, &num_config);
+    assert(EGL_FALSE != result);
 
-   // create an EGL rendering context
-   context = eglCreateContext(display, config, EGL_NO_CONTEXT, NULL);
-   assert(context!=EGL_NO_CONTEXT);
+    // create an EGL rendering context
+    context = eglCreateContext(display, config, EGL_NO_CONTEXT, NULL);
+    assert(context!=EGL_NO_CONTEXT);
 
-   // create an EGL window surface
-   int32_t success = graphics_get_display_size(0 /* LCD */, &screen_width, &screen_height);
-   assert( success >= 0 );
+    // create an EGL window surface
+    int32_t success = graphics_get_display_size(0 /* LCD */, &screen_width, &screen_height);
+    assert( success >= 0 );
 
-   VC_RECT_T dst_rect = {.x = 0, .y = 0, .width = screen_width, .height = screen_height};
-   VC_RECT_T src_rect = {.x = 0, .y = 0, .width = screen_width<<16, .height = screen_height<<16};
+    VC_RECT_T dst_rect = {.x = 0, .y = 0, .width = screen_width, .height = screen_height};
+    VC_RECT_T src_rect = {.x = 0, .y = 0, .width = screen_width<<16, .height = screen_height<<16};
 
-   dispman_display = vc_dispmanx_display_open(0 /* LCD */);
-   dispman_update = vc_dispmanx_update_start(0);
+    dispman_display = vc_dispmanx_display_open(0 /* LCD */);
+    dispman_update = vc_dispmanx_update_start(0);
 
-   dispman_element = vc_dispmanx_element_add (dispman_update, dispman_display,
-      0/*layer*/, &dst_rect, 0/*src*/,
-      &src_rect, DISPMANX_PROTECTION_NONE, 0 /*alpha*/, 0/*clamp*/, 0/*transform*/);
+    dispman_element = vc_dispmanx_element_add (dispman_update, dispman_display,
+                                               0/*layer*/, &dst_rect, 0/*src*/,
+                                               &src_rect, DISPMANX_PROTECTION_NONE, 0 /*alpha*/, 0/*clamp*/, 0/*transform*/);
 
-   nativewindow.element = dispman_element;
-   nativewindow.width = screen_width;
-   nativewindow.height = screen_height;
-   vc_dispmanx_update_submit_sync(dispman_update);
+    nativewindow.element = dispman_element;
+    nativewindow.width = screen_width;
+    nativewindow.height = screen_height;
+    vc_dispmanx_update_submit_sync(dispman_update);
 
-   surface = eglCreateWindowSurface(display, config, &nativewindow, NULL);
-   if(surface == EGL_NO_SURFACE)
-   {
-      fprintf(stderr, "eglCreateWindowSurface returned ELG_NO_SURFACE.  "
-        "Try closing other OpenGL programs.\n");
-      exit(1);
-   }
+    surface = eglCreateWindowSurface(display, config, &nativewindow, NULL);
+    if(surface == EGL_NO_SURFACE)
+    {
+        fprintf(stderr, "eglCreateWindowSurface returned ELG_NO_SURFACE.  "
+                "Try closing other OpenGL programs.\n");
+        exit(1);
+    }
 
-   // connect the context to the surface
-   result = eglMakeCurrent(display, surface, surface, context);
-   assert(EGL_FALSE != result);
+    // connect the context to the surface
+    result = eglMakeCurrent(display, surface, surface, context);
+    assert(EGL_FALSE != result);
 
-   glClearColor(0, 0, 0, 0.5);   // set background colors
-   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear buffers
-   glShadeModel(GL_FLAT);
+    glClearColor(0, 0, 0, 0.5);   // set background colors
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear buffers
+    glShadeModel(GL_FLAT);
 
-   // Enable back face culling.
-   glEnable(GL_CULL_FACE);
+    // Enable back face culling.
+    glEnable(GL_CULL_FACE);
 }
 
 // Description: Sets the OpenGL|ES model to default values
 static void init_projection()
 {
-   float nearp = 1, farp = 500.0f, hht, hwd;
+    float nearp = 1, farp = 500.0f, hht, hwd;
 
-   glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
 
-   glViewport(0, 0, (GLsizei)screen_width, (GLsizei)screen_height);
+    glViewport(0, 0, (GLsizei)screen_width, (GLsizei)screen_height);
 
-   glMatrixMode(GL_PROJECTION);
-   glLoadIdentity();
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
 
-   hht = nearp * tan(45.0 / 2.0 / 180.0 * M_PI);
-   hwd = hht * screen_width / screen_height;
+    hht = nearp * tan(45.0 / 2.0 / 180.0 * M_PI);
+    hwd = hht * screen_width / screen_height;
 
-   glFrustumf(-hwd, hwd, -hht, hht, nearp, farp);
+    glFrustumf(-hwd, hwd, -hht, hht, nearp, farp);
 }
 
 static void init_textures(void)
 {
-   glEnable(GL_TEXTURE_2D);
-   model_board_init();
+    glEnable(GL_TEXTURE_2D);
+    model_board_init();
 }
 
 static void redraw_scene()
 {
-   // Start with a clear screen
-   glClear(GL_COLOR_BUFFER_BIT);
+    // Start with a clear screen
+    glClear(GL_COLOR_BUFFER_BIT);
 
-   // Move the camera back so we can see the board.
-   glMatrixMode(GL_MODELVIEW);
-   glLoadIdentity();
-   glTranslatef(0, 0, -30);
+    // Move the camera back so we can see the board.
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glTranslatef(0, 0, -30);
 
-   // Convert screen coords (x=right y=up z=out) to ground coords (x=east y=north z=up).
-   glRotatef(-90, 1, 0, 0);
-   glRotatef(screen_orientation, 0, 0, 1);
+    // Convert screen coords (x=right y=up z=out) to ground coords (x=east y=north z=up).
+    glRotatef(-90, 1, 0, 0);
+    glRotatef(screen_orientation, 0, 0, 1);
 
-   // Convert ground coords to board coordinates.
-   glMultMatrixf(matrix[0]);
+    // Convert ground coords to board coordinates.
+    glMultMatrixf(matrix[0]);
 
-   model_board_redraw(acceleration, magnetic_field);
+    model_board_redraw(acceleration, magnetic_field);
 
-   eglSwapBuffers(display, surface);
+    eglSwapBuffers(display, surface);
 }
 
 static void exit_func(void)
 {
-   // clear screen
-   glClear(GL_COLOR_BUFFER_BIT);
-   eglSwapBuffers(display, surface);
+    // clear screen
+    glClear(GL_COLOR_BUFFER_BIT);
+    eglSwapBuffers(display, surface);
 
-   // Release OpenGL resources
-   eglMakeCurrent(display, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
-   eglDestroySurface(display, surface);
-   eglDestroyContext(display, context);
-   eglTerminate(display);
+    // Release OpenGL resources
+    eglMakeCurrent(display, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
+    eglDestroySurface(display, surface);
+    eglDestroyContext(display, context);
+    eglTerminate(display);
 }
 
 /* The matrix expected on the standrd input is a 
